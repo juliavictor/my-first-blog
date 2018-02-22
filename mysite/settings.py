beb83e8c536c821cb4114f9b8ba26d1035efea8d
Yulia <solomonovajulia@gmail.com>
2018-02-22 11:48:19 +0300

    Added text over images & gradient

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,11 +121,6 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 # Images
 MEDIA_ROOT = (
 os.path.join(BASE_DIR, 'images')
