124f819f0d6d90554ad703a09f8f920e85e48bae
Yulia <solomonovajulia@gmail.com>
2018-03-13 13:07:07 +0300

    New posts & random posts layout

--- blog/views.py
+++ blog/views.py
@@ -13,22 +13,29 @@ def post_list(request):
     if not request.session.session_key:
         request.session.save()
 
-    sum_values = recs.sum(axis=0).astype(str).sort_values(ascending=False)
-    filter = sum_values.str.contains("[a-z]")
-    sum_values = sum_values[~filter]
+    #sum_values = recs.sum(axis=0).astype(str).sort_values(ascending=False)
+    #filter = sum_values.str.contains("[a-z]")
+    #sum_values = sum_values[~filter]
 
     # print(sum_values, file=sys.stderr)
 
-    # collect session data on this user
-    session_data = recs.loc[recs['session_id'] == request.session.session_key]
-    if not session_data.empty:
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-        for post in session_data.columns[(session_data == 1).iloc[0]]:
-            if post != 'session_id':
-                posts = posts.exclude(id=post).filter(published_date__lte=timezone.now()).order_by('published_date')
-    else:
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # excluding watched posts
+    #session_data = recs.loc[recs['session_id'] == request.session.session_key]
+    #if not session_data.empty:
+    #    posts = Post.objects
+    #    for post in session_data.columns[(session_data == 1).iloc[0]]:
+    #        if post != 'session_id':
+    #           posts = posts.exclude(id=post)
+    #else:
+    #    posts = Post.objects
+    posts = Post.objects
+
+    # sorting by desc and only 8 first popular posts
+
+    posts = [x for x in posts.filter(published_date__lte=timezone.now()).order_by('-views')[:6]] + \
+            [y for y in posts.filter(published_date__lte=timezone.now()).order_by('-published_date')[:2]]
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('?')[:8]
 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -49,6 +56,8 @@ def post_detail(request, pk):
     # Unique views counter setting
     post.views = recs[pk].sum()
     post.save()
+    # For black & white filter
+    request.session[pk] = 1
     return render(request, 'blog/post_detail.html', {'post': post})
 
 @login_required
