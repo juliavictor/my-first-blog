d60b6104b18ef7f937af119da7c6343515d8c4f5
Yulia <solomonovajulia@gmail.com>
2018-04-19 14:58:42 +0300

    New quote model & updated poll model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Post, Comment, Poll
+from .models import Post, Comment, Poll, Quote, PostAdmin
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Poll)
+admin.site.register(Quote)
+
+
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.utils import timezone
 from django import forms
 from django.core.validators import MinValueValidator
+from django.contrib import admin
+from django.forms import TextInput, Textarea
+
 
 STATUS_CHOICES = (
     (1, ("Язык и речь")),
@@ -22,6 +25,22 @@ FA_CHOICES = (
     (0, ("Против")),
     (1, ("За")))
 
+
+def split_text(full_name):
+    split = full_name.split()
+    text = ""
+    for word in split[:7]:
+        text += word + " "
+    if len(split) > 7:
+        text += "..."
+    return text
+
+
+# Field sizes
+glob_field_rows = 4
+glob_field_cols = 80
+
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -34,7 +53,6 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
     image = models.ImageField(upload_to='img', default="default-image.png")
-
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
 
@@ -67,6 +85,48 @@ class Poll(models.Model):
     post = models.ForeignKey('blog.Post', related_name='polls')
     question = models.TextField()
     polarity = models.IntegerField(choices=FA_CHOICES, default=1)
-
     def __str__(self):
-        return self.question
+        return split_text(self.question)
+
+
+
+class Quote(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='quotes')
+    quote = models.TextField()
+    author = models.TextField()
+    def __str__(self):
+        return split_text(self.quote)
+
+
+class PollInline(admin.StackedInline):
+    model = Poll
+    extra = 1
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': glob_field_cols})},
+        models.TextField: {'widget': Textarea(attrs={'cols': glob_field_cols,
+                                                     'rows': glob_field_rows})},
+    }
+
+class QuoteInline(admin.StackedInline):
+    model = Quote
+    extra = 1
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': glob_field_cols})},
+        models.TextField: {'widget': Textarea(attrs={'cols': glob_field_cols,
+                                                     'rows': glob_field_rows})},
+    }
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        PollInline,
+        QuoteInline,
+    ]
+
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': glob_field_cols})},
+        models.TextField: {'widget': Textarea(attrs={'cols': glob_field_cols,
+                                                     'rows': glob_field_rows})},
+    }
+
+
