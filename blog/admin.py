3fa84a2c0fa0efc2a433214b606034ce2e562676
Yulia <solomonovajulia@gmail.com>
2019-02-19 14:18:44 +0300

    Updated Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Post, Comment, Poll, Quote, PostAdmin
+from .models import Post, Comment, Poll, Quote, PostAdmin, QuoteAdmin
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Poll)
-admin.site.register(Quote)
+admin.site.register(Quote, QuoteAdmin)
 
 
 
